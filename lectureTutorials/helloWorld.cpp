0dc5fed7e397780062e3b4ea83e4fc930c656e9e
Lewis <mlewis8@RVLAS033L-03741.local>
2017-08-30 18:36:47 -0500

    Week one, C++ tutorial file, and compile/executable commands

---
+++ lectureTutorials/helloWorld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int value1;
+	int value2;
+	int value3
+
+	string valueString1 = "Hello ";
+	string valueString2 = "World";
+	string value3String = valueString1 + valueString2;
+
+	cout << " Hello World " << endl;
+	
+
+
+
+}
